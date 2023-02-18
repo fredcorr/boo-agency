021c2104e3322390e90d8d665b5809d52e82581c
Federico Corradi <Federico Corradi>
2023-02-18 19:17:56 +0000

    Create basic schema for CMS

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,1 +1,4 @@
-export const schemaTypes = []
+import Page from './documents/Page'
+import {ContentBuilder, Image, Seo } from './objects'
+
+export const schemaTypes = [ContentBuilder, Page, Image, Seo]
