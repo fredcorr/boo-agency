fd7ef9ea95cd339331e1d720bd2679cf775d2a74
Federico Corradi <Federico Corradi>
2023-02-20 19:13:35 +0000

    Set up folder structure

---
+++ sanity/schemas/objects/components/Services.ts
@@ -0,0 +1,18 @@
+export const QuickLinks = {
+  title: 'Services',
+  name: 'services',
+  type: 'object',
+  fields: [
+    {
+      title: 'Headline',
+      name: 'headline',
+      type: 'string',
+    },
+    {
+      title: 'Links',
+      name: 'links',
+      type: 'array',
+      of: [],
+    },
+  ],
+}
